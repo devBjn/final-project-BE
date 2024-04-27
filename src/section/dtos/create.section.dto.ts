3fa04493749bb217cdc10afd475f3559641e4fb0
devBjn <tamnguyen.10092000tv@gmail.com>
2024-04-27 19:03:45 +0700

    check

--- src/section/dtos/create.section.dto.ts
+++ src/section/dtos/create.section.dto.ts
@@ -1,17 +1,8 @@
 import { IsString } from 'class-validator';
-import { Task } from 'src/task/entity/task.entity';
 
 export class CreateSectionRequest {
   @IsString()
   title: string;
 
   tasks?: string[];
-}
-
-export class GetSectionResponse {
-  id: number;
-
-  title: string;
-
-  tasks?: Task[];
 }
